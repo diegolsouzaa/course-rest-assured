2e31a16d8441c32f7b2a3cded482655f7491102f
diego souza <diegolsouzaa@gmail.com>
2020-11-03 23:05:05 -0300

    Validations with token

--- src/main/java/rest/AuthTest.java
+++ src/main/java/rest/AuthTest.java
@@ -1,8 +1,13 @@
 package rest;
 
+import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
 import org.hamcrest.Matchers;
 import org.hamcrest.Matchers.*;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -91,5 +96,75 @@ public class AuthTest {
                 .statusCode(200)
                 .body("status", Matchers.is("logado"))
         ;
+    }
+
+    @Test
+    public void deveFazerAutenticacaoComTokenJWT(){
+        Map<String, String> login = new HashMap<String, String>();
+        login.put("email", "diego@lauriano");
+        login.put("senha", "1234");
+
+        String token =
+        given()
+                .log().all()
+                .body(login)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("http://barrigarest.wcaquino.me/signin")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract().path("token")
+                ;
+        given()
+                .log().all()
+                .header("Authorization", "JWT " + token)
+                .when()
+                .get("http://barrigarest.wcaquino.me/contas")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("nome", Matchers.hasItem("Conta de teste"))
+        ;
+    }
+
+    @Test
+    public void deveAcessarAplicacaoWeb(){
+
+        String cookie =
+        //login
+        given()
+                .log().all()
+                .formParam("email", "diego@lauriano")
+                .formParam("senha", "1234")
+                .contentType(ContentType.URLENC.withCharset("UTF-8"))
+                .when()
+                .post("http://seubarriga.wcaquino.me/logar")
+                .then()
+                .log().all()
+                .extract().header("set-cookie");
+
+
+        cookie = cookie.split("=")[1].split(";")[0];
+        System.out.println(cookie);
+
+        //obter conta
+        String body =
+        given()
+                .log().all()
+                .cookie("connect.sid", cookie)
+                .when()
+                .get("http://seubarriga.wcaquino.me/contas")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("html.body.table.tbody.tr[0].td[0]", Matchers.is("Conta de teste"))
+                .extract().body().asString();
+
+
+        System.out.println("------------------");
+        XmlPath xmlPath = new XmlPath(XmlPath.CompatibilityMode.HTML, body);
+        System.out.println(xmlPath.getString("html.body.table.tbody.tr[0].td[0]"));
+
     }
 }
